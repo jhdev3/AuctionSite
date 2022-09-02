2151387b3f09caa256a8cfbfe8d9d89c38fcd650
Jacob Hedén <91886824+jhdev3@users.noreply.github.com>
2022-09-02 11:54:47 +0200

    Done With Create API and Postman
    Using Beginform-ajax to validate and some coustome javascript onsuccess or oncompleted doesnt matter beacuse i am not using error codes i want to display a propper error message :+1:
    
    And incase of succes i realod page beacuse thats the easiest way to show new createted categories :)

--- AuctionSite/Areas/Admin/Controllers/ManagerCategoryController.cs
+++ AuctionSite/Areas/Admin/Controllers/ManagerCategoryController.cs
@@ -31,33 +31,36 @@ namespace AuctionSite.Areas.Admin.Controllers
             return View();
         }
 
-        [HttpPost, ActionName("Create")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> CreatePartial(Category category)
-        {
-            if (ModelState.IsValid)
-            {
-                //If Name already exist.
-                var test = await _db.Categories.Where(c => c.CategoryName == category.CategoryName).FirstOrDefaultAsync();
-                if (test != null) //If it exists
-                {
-                    ModelState.AddModelError("", $"Kategorin: {category.CategoryName} finns redan");
-                    TempData["Error"] = $"Kategorin: {category.CategoryName} finns redan";
-                    //return Json(new
-                    //{
-                    //    status = "failure",
-                    //    formErrors = ModelState.Select(kvp => new { key = kvp.Key, errors = kvp.Value.Errors.Select(e => e.ErrorMessage) })
-                    //});
-                    return View("_CreatePartial", category);
 
-                }
-                _db.Categories.Add(category);
-                await _db.SaveChangesAsync();
-                TempData["Success"] = $"Lyckades skapa : {category.CategoryName} :)";
-                return RedirectToAction(nameof(Index));
-            }
-            return View("_CreatePartial", category);
-        }
+        ////Make This An API Create End Points for the Failure for the stuff :) Asp-Validation all works :). 
+        //[HttpPost, ActionName("Create")]
+        //[ValidateAntiForgeryToken]
+        //public async Task<IActionResult> CreatePartial(Category category)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        //If Name already exist.
+        //        var test = await _db.Categories.Where(c => c.CategoryName == category.CategoryName).FirstOrDefaultAsync();
+        //        if (test != null) //If it exists
+        //        {
+        //            ModelState.AddModelError("", $"Kategorin: {category.CategoryName} finns redan");
+        //            TempData["Error"] = $"Kategorin: {category.CategoryName} finns redan";
+        //            //return Json(new
+        //            //{
+        //            //    status = "failure",
+        //            //    formErrors = ModelState.Select(kvp => new { key = kvp.Key, errors = kvp.Value.Errors.Select(e => e.ErrorMessage) })
+        //            //});
+        //           return BadRequest(ModelState);
+
+        //        }
+        //        _db.Categories.Add(category);
+        //        await _db.SaveChangesAsync();
+        //        TempData["Success"] = $"Lyckades skapa : {category.CategoryName} :)";
+        //        return RedirectToAction(nameof(Index));
+        //    }
+
+        //    return View("_CreatePartial", category);
+        //}
 
         // GET: ManagerCategoryController/Edit/5
         public ActionResult Edit(int id)
@@ -100,5 +103,50 @@ namespace AuctionSite.Areas.Admin.Controllers
                 return View();
             }
         }
+
+
+
+
+
+
+
+        #region API CALLS
+        /// <summary>
+        /// Post function of creat a Category and save it to database,
+        /// will return JSON object of succes and incase of insucces modelstate errors with that.
+        /// </summary>
+        /// <param name="category">Model Category </param>
+        /// <returns>Json object{ succes, ModelError}</returns>
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                //Check if category already exist.
+                var test = await _db.Categories.Where(c => c.CategoryName == category.CategoryName).FirstOrDefaultAsync();
+                if (test != null) //If it exists
+                {
+                    ModelState.AddModelError("", $"The Category: {category.CategoryName} already exists");
+                    return Json(new
+                    {
+                        success = false,
+                        ModelError = ModelState.Values.SelectMany(x => x.Errors)
+                    });
+                }
+                category.CategoryName = char.ToUpper(category.CategoryName[0]) + category.CategoryName.Substring(1).ToLower(); //Make Categoriged like a title. :)
+                _db.Categories.Add(category);
+                await _db.SaveChangesAsync();
+                return Json (new { Success = true });
+            }
+            return Json(new
+            {
+                success = false,
+                ModelError = ModelState.Values.SelectMany(x => x.Errors)
+            });
+        }
     }
+
+    #endregion
 }
+
--- AuctionSite/Models/Category.cs
+++ AuctionSite/Models/Category.cs
@@ -5,6 +5,8 @@ namespace AuctionSite.Models
     public class Category : DBentity
     {
         [Required(ErrorMessage = "Field can't be empty!")]
+        [StringLength(50, MinimumLength = 2,
+            ErrorMessage = "{0} cannot exceed {1} characters and not be smaller then {2} characters")]
         public string CategoryName { get; set; }    
 
     }
